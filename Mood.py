1649babaf19eb670a02d512eabf4c785b5ae9404
Sanjay <43458005+scsewbh@users.noreply.github.com>
2020-11-19 23:14:39 -0500

    Updated to Support GUI
    GUI made with PyQt5 connected to Moodle to allow download of courses.

--- Mood.py
+++ Mood.py
@@ -3,31 +3,31 @@ from bs4 import BeautifulSoup as bs
 import os
 import re
 
-current_data_keys = {"C364": "32127", "C438": "32126", "C456": "32123"}
-pass_data_keys = {"C258": "23594", "C312": "30179", "C334": "30176", "C341": "30173", "C353": "28314", "C360": "25905", "C420": "25901", "C439": "28309"} 
-
-#Username and Password for Moodle
-USERNAME = ''
-PASSWORD = ''
-
-
 moodle_url = 'https://lms.manhattan.edu/my/'
-login_page = 'https://auth.manhattan.edu/idp/profile/cas/login?execution=e1s1&j_username='+ USERNAME +'&j_password='+ PASSWORD +'&_eventId_proceed='
+
+#Have to been signed in
 course_base_link = 'https://lms.manhattan.edu/course/view.php?id='  # append course codes at end of url to get course page (5-digits code) (data-keys)
 download_link_addon = '&redirect=1'
+profile_link = 'https://lms.manhattan.edu/user/profile.php?id=&showallcourses=1'
 
 class Moodle:
     def __init__(self):
+        #Username and Password for Moodle
         self.session = requests.Session()
         self.session.get(moodle_url)
         self.course_title = ''
         self.dictCourseSel = {}
-        r = self.session.get(login_page)
-        if r.status_code == 200:
+
+    def login(self, Username, Password):
+        self.login_page = 'https://auth.manhattan.edu/idp/profile/cas/login?execution=e1s1&j_username='+ Username +'&j_password='+ Password +'&_eventId_proceed='
+        r = self.session.get(self.login_page)
+        if r.status_code == 200 and r.url == moodle_url:
             print("Successful Login")
+            return True
         else:
             print("Failed Login")
-    
+            return False
+
     def gatherAllCourses(self):
         course_details_prof = self.session.get(profile_link)
         soup = bs(course_details_prof.text, 'html.parser')
@@ -37,19 +37,27 @@ class Moodle:
             m = re.search('(?<=course=).*(?=&)', str(elem))
             val = m.group(0)
             self.dictCourseSel[elem.text] = val
-    
+
     def findCourses(self, courseKey):
         course_link = course_base_link + courseKey
         courseUnparsed = self.session.get(course_link)
         return courseUnparsed
 
-    def parsingCourseData(self, dataUnparsed):
+    def parsingCourseData(self, dataUnparsed, path_start):
         soup = bs(dataUnparsed.text, 'html.parser')
         # for all activity instances check title name for File
         self.course_title = soup.find('div', {'class': 'page-header-headings'}).text
-        os.mkdir('./' + self.course_title) 
+        self.course_title = re.sub(':', '-', self.course_title)
+        if os.path.isdir(path_start + '/MoodleDL'):
+            pass
+        else:
+            os.mkdir(path_start + '/MoodleDL')
+        if os.path.isdir(path_start + '/MoodleDL/' + self.course_title):
+            pass
+        else:
+            os.mkdir(path_start + '/MoodleDL/' + self.course_title)
+        self.path = path_start + '/MoodleDL/' + self.course_title
         activityInstances = soup.findAll('div', {'class': 'activityinstance'})
-        print(activityInstances)
         course_data = {}
         for ai in activityInstances:   #Can change into a dictionary to match title to href instead of two separate arrays
             hrefInstance = ai.find('a', href=True).get('href')
@@ -58,14 +66,13 @@ class Moodle:
                 fileType = (title.find('span').text).strip()
             except AttributeError:
                 fileType = 'Other'
-            print(fileType)
             if fileType == 'File':
                 course_data[title.text] = [hrefInstance, fileType]
         return course_data
         # Forum, File (ppt,pdf,etc.), if none just a link, Assignment (HW), Page (Intructions)
         # Resources - ppt and docs and pdfs
         # Assignments/Project/Quiz
-
+    
     def downloadFiles(self, course_data):
         for key in course_data:
             downloadURL = course_data[key][0] + download_link_addon
@@ -73,7 +80,8 @@ class Moodle:
             redirect_url = r.url
             if redirect_url.find('/'):
                 filename = redirect_url.split('/')[-1]
-                new_filename = self.course_title + '/' + filename
+                filename = re.sub('%20', ' ', filename)
+                new_filename = self.path + '/' + filename
                 open(new_filename, 'wb').write(r.content)
 
     def assignments(self, dataUnparsed):
@@ -97,11 +105,7 @@ class Moodle:
                 else:
                     homeworks[titleName] = "Friday, October 16, 2025, 11:00 PM", hrefInstance
         return homeworks
+    
 
-for key in pass_data_keys:
-    instant = Moodle()
-    courseData = instant.findCourses(pass_data_keys[key])
-    x = instant.parsingCourseData(courseData)
-    instant.downloadFiles(x)
 #instant.assignments(courseData)
 
